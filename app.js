0142dbc240bf843cac68a2c08529d5ff90ddf612
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-12-04 14:21:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+//JQuery for flipping over function on the image
+$('.f1_container').click(function () {
+    $(this).toggleClass('active');
+});
+
+
+
+
+
+
+//https://codepen.io/Bjornros/pen/VPzQzB
+
+//https://travishorn.com/responsive-grid-in-2-minutes-with-css-grid-layout-4842a41420fe
